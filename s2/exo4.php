9e292a1077e13bd7f4655461bb12d3465bb77edf
kazcodepro <kazcodepro@gmail.com>
2026-03-25 15:59:43 +0100

    php season2

--- s2/exo4.php
+++ s2/exo4.php
@@ -1,6 +1,15 @@
 <?php 
     
+    $sPhraseFinal = "";
 
+    if (isset($_POST["text-area-result"])) {
+        $sPhrase1 = "belle marquise, vos beaux yeux me font mourir d’amour";
+        $sPhrase2 = "vos beaux yeux me font mourir d’amour, belle marquise";
+        $sPhrase3 = "vos beaux yeux, belle marquise, me font mourir d’amour";
+        $sPhrase4 = "me font mourir d’amour, belle marquise, vos beaux yeux";
+
+        $sPhraseFinal = $sPhrase1 . "\n" . $sPhrase2 ."\n" . $sPhrase3 . "\n" . $sPhrase4;
+    }
 
     require "exo4.html";
 ?>
