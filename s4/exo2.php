afe1dbb1b9b431eb1090fb1fd1ff67bf49e0ea2c
kazcodepro <kazcodepro@gmail.com>
2026-03-25 16:51:52 +0100

    debut php season4 TO DO

--- s4/exo2.php
+++ s4/exo2.php
@@ -1,4 +1,23 @@
 <?php 
+    $sMessage = "";
+
+    if (isset($_POST["text-area-input"])) {
+        $iHeures = $_POST["text-area-input"];
+        $iMinutes = $_POST["text-area-input2"];
+
+        $iMinutes++;
+
+        if ($iMinutes >= 60) {
+            $iMinutes = 0;
+            $iHeures ++;
+        }
+
+        if ($iHeures >= 24) {
+            $iHeures = 0;
+        }
+
+        $sMessage = "PHP Dans une minute, il sera " . $iHeures . " heure(s) " . $iMinutes;
+    }
     require "exo2.html";
 ?>
 
